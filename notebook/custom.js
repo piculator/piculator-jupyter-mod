c5016061362e23deaebea51425641c012e62c300
kxxt <18085551+kxxt@users.noreply.github.com>
2021-02-03 11:50:53 +0800

    Initial commit

---
+++ notebook/custom.js
@@ -0,0 +1,111 @@
+// leave at least 2 line with only a star on it below, or doc generation fails
+/**
+ *
+ *
+ * Placeholder for custom user javascript
+ * mainly to be overridden in profile/static/custom/custom.js
+ * This will always be an empty file in IPython
+ *
+ * User could add any javascript in the `profile/static/custom/custom.js` file.
+ * It will be executed by the ipython notebook at load time.
+ *
+ * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
+ *
+ *
+ * The object available at load time depend on the version of IPython in use.
+ * there is no guaranties of API stability.
+ *
+ * The example below explain the principle, and might not be valid.
+ *
+ * Instances are created after the loading of this file and might need to be accessed using events:
+ *     define([
+ *        'base/js/namespace',
+ *        'base/js/promises'
+ *     ], function(IPython, promises) {
+ *         promises.app_initialized.then(function (appName) {
+ *             if (appName !== 'NotebookApp') return;
+ *             IPython.keyboard_manager....
+ *         });
+ *     });
+ *
+ * __Example 1:__
+ *
+ * Create a custom button in toolbar that execute `%qtconsole` in kernel
+ * and hence open a qtconsole attached to the same kernel as the current notebook
+ *
+ *    define([
+ *        'base/js/namespace',
+ *        'base/js/promises'
+ *    ], function(IPython, promises) {
+ *        promises.app_initialized.then(function (appName) {
+ *            if (appName !== 'NotebookApp') return;
+ *            IPython.toolbar.add_buttons_group([
+ *                {
+ *                    'label'   : 'run qtconsole',
+ *                    'icon'    : 'icon-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
+ *                    'callback': function () {
+ *                        IPython.notebook.kernel.execute('%qtconsole')
+ *                    }
+ *                }
+ *                // add more button here if needed.
+ *                ]);
+ *        });
+ *    });
+ *
+ * __Example 2:__
+ *
+ * At the completion of the dashboard loading, load an unofficial javascript extension
+ * that is installed in profile/static/custom/
+ *
+ *    define([
+ *        'base/js/events'
+ *    ], function(events) {
+ *        events.on('app_initialized.DashboardApp', function(){
+ *            requirejs(['custom/unofficial_extension.js'])
+ *        });
+ *    });
+ *
+ *
+ *
+ * @module IPython
+ * @namespace IPython
+ * @class customjs
+ * @static
+ */
+console.log("Custom.js LOADED!");
+define([
+    'base/js/events'
+], function (events) {
+    events.on('app_initialized.DashboardApp', function () {
+        requirejs(['custom/piculator-mod.js'])
+    });
+});
+function ResetLayout() {
+    var path = window.location.pathname;
+    if (path.search('/notebooks/') != -1) {
+        var maintoolbar = document.getElementById('maintoolbar');
+        var save_widget = document.getElementById('save_widget');
+        var kernel_logo = document.getElementById('kernel_logo_widget')
+        var menubar = document.getElementsByClassName('navbar-collapse collapse')[0];
+        save_widget.parentNode.insertBefore(maintoolbar, kernel_logo);
+        menubar.appendChild(save_widget);
+    }else if(path.search('/tree')!=-1){
+        console.log("TREE!")
+        var new_menu = document.getElementById('new-menu');
+        new_menu.classList.remove('dropdown-menu');
+        new_menu.classList.add('new-menu');
+        var notebook_toolbar = document.getElementById('notebook_toolbar');
+        var nb = document.getElementById('notebooks');
+        nb.appendChild(new_menu);
+        nb.insertBefore(new_menu,notebook_toolbar);
+    }
+
+}
+window.addEventListener('load', function () {
+    //console.log("Loaded!");
+    ResetLayout();
+});
+document.onload = function () {
+    //console.log('Document Loaded!');
+    ResetLayout();
+}
